41e9f2b7eb4acc6027d26f9d68c3ecc70eea5964
dragicalj <gagaljubisavljevic111@gmail.com>
2022-07-06 18:00:19 +0200

    Kreirana klasa Start u kojoj se instancira i pokrece glavna forma za pokretanje servera

---
+++ src/main/java/rs/ac/bg/fon/nprog/start/Start.java
@@ -0,0 +1,10 @@
+package rs.ac.bg.fon.nprog.start;
+
+import rs.ac.bg.fon.nprog.forme.FormaMain;
+
+public class Start {
+	
+	public static void main(String[] args) {
+        new FormaMain().setVisible(true);
+    }
+}
