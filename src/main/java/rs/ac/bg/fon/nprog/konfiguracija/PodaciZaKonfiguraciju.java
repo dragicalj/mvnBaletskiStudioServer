d70904b6d5c82252587d17211e56c4539e71c3d6
dragicalj <gagaljubisavljevic111@gmail.com>
2022-07-06 18:11:32 +0200

    Dodate potrebne varijable u klasu PodaciZaKonfiguraciju

--- src/main/java/rs/ac/bg/fon/nprog/konfiguracija/PodaciZaKonfiguraciju.java
+++ src/main/java/rs/ac/bg/fon/nprog/konfiguracija/PodaciZaKonfiguraciju.java
@@ -3,5 +3,8 @@ package rs.ac.bg.fon.nprog.konfiguracija;
 public class PodaciZaKonfiguraciju {
 	
     public static final String PATH = "C:\\Users\\Korisnik\\eclipse-workspace\\mvnServer\\dbconfig.properties";
+    public static final String URL="url";
+    public static final String USERNAME="username";
+    public static final String PASSWORD = "password";
     
 }
