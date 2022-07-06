6086e9977a9d54d8fa36552236365355013957f8
dragicalj <gagaljubisavljevic111@gmail.com>
2022-07-06 18:04:54 +0200

    Kreirana klasa PodaciZaKonfiguraciju i varijabla koja predstavlja putanju do konfiguracionog fajla

---
+++ src/main/java/rs/ac/bg/fon/nprog/konfiguracija/PodaciZaKonfiguraciju.java
@@ -0,0 +1,7 @@
+package rs.ac.bg.fon.nprog.konfiguracija;
+
+public class PodaciZaKonfiguraciju {
+	
+    public static final String PATH = "C:\\Users\\Korisnik\\eclipse-workspace\\mvnServer\\dbconfig.properties";
+    
+}
