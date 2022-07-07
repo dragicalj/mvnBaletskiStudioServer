5fafc5e27d6cdf69311799d9401cbf55871cb318
dragicalj <gagaljubisavljevic111@gmail.com>
2022-07-07 18:52:42 +0200

    Kreirana klasa PromeniKoreografaSO, u njoj implementirane sve metode koje su potrebne za ovu sistemsku operaciju

---
+++ src/main/java/rs/ac/bg/fon/nprog/operacije/koreograf/PromeniKoreografaSO.java
@@ -0,0 +1,30 @@
+package rs.ac.bg.fon.nprog.operacije.koreograf;
+
+import java.util.Date;
+
+import rs.ac.bg.fon.nprog.domen.ApstraktniDomenskiObjekat;
+import rs.ac.bg.fon.nprog.domen.Koreograf;
+import rs.ac.bg.fon.nprog.operacije.ApstraktnaSO;
+
+public class PromeniKoreografaSO extends ApstraktnaSO{
+
+    @Override
+    protected void precondition(Object param) throws Exception {
+        if (!(param instanceof Koreograf)) {
+            throw new Exception("Prosledjeni objekat nije instanca klase Koreograf!");
+        }
+        Koreograf koreograf=(Koreograf) param;
+
+        if(koreograf.getDatumRodjenja().after(new Date())
+                || koreograf.getDatumRodjenja().equals(new Date())){
+            throw new Exception("Datum rodjenja mora biti pre danasnjeg datuma!");
+        }
+    }
+
+    @Override
+    protected void executeOperation(Object param) throws Exception {
+        repository.promeni((ApstraktniDomenskiObjekat) param);
+    }
+    
+}
+
