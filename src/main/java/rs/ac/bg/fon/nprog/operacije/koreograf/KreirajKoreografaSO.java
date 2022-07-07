dbd6b3d886670b7b4b384033d24098f33fc39d57
dragicalj <gagaljubisavljevic111@gmail.com>
2022-07-07 10:28:49 +0200

    Kreirana klasa KreirajKoreografaSO, u njoj implementirane operacije koje su potrebne za ovu sistemsku operaciju

---
+++ src/main/java/rs/ac/bg/fon/nprog/operacije/koreograf/KreirajKoreografaSO.java
@@ -0,0 +1,41 @@
+package rs.ac.bg.fon.nprog.operacije.koreograf;
+
+import java.sql.Date;
+
+import rs.ac.bg.fon.nprog.domen.ApstraktniDomenskiObjekat;
+import rs.ac.bg.fon.nprog.domen.Koreograf;
+import rs.ac.bg.fon.nprog.operacije.ApstraktnaSO;
+
+public class KreirajKoreografaSO extends ApstraktnaSO{
+
+    long indeks=-1;
+
+    public KreirajKoreografaSO() {
+        super();
+    }
+   
+    
+    @Override
+    protected void precondition(Object param) throws Exception {
+        if (!(param instanceof Koreograf)) {
+            throw new Exception("Prosledjeni objekat nije instanca klase Koreograf!");
+        }
+        Koreograf koreograf=(Koreograf) param;
+
+        if(koreograf.getDatumRodjenja().after(new java.util.Date())
+                || koreograf.getDatumRodjenja().equals(new java.util.Date())){
+            throw new Exception("Datum rodjenja mora biti pre danasnjeg datuma!");
+        }
+        
+    }
+
+    @Override
+    protected void executeOperation(Object param) throws Exception {
+        indeks=repository.kreirajSlog((ApstraktniDomenskiObjekat)param);
+    }
+
+    public long getIndeks() {
+        return indeks;
+    }
+
+}
