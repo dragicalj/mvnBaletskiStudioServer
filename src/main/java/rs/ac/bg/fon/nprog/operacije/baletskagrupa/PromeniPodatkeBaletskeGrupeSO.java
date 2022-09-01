717f3fd5f3f171fffba5e25db54db38ea8164b83
dragicalj <gagaljubisavljevic111@gmail.com>
2022-09-02 01:54:08 +0200

    Dokumentovana klasa PromeniPodatkeBaletskeGrupeSO

--- src/main/java/rs/ac/bg/fon/nprog/operacije/baletskagrupa/PromeniPodatkeBaletskeGrupeSO.java
+++ src/main/java/rs/ac/bg/fon/nprog/operacije/baletskagrupa/PromeniPodatkeBaletskeGrupeSO.java
@@ -4,7 +4,17 @@ import rs.ac.bg.fon.nprog.domen.ApstraktniDomenskiObjekat;
 import rs.ac.bg.fon.nprog.domen.BaletskaGrupa;
 import rs.ac.bg.fon.nprog.operacije.ApstraktnaSO;
 import rs.ac.bg.fon.nprog.repository.Repository;
-
+/**
+ * 
+ * Klasa koja predstavlja sistemsku operaciju za promenu podataka postojece baletske grupe. Operacija
+ * se izvrsava pozivom odgovarajuceg upita nad bazom podataka i nema nikavu povratnu vrednost.
+ * 
+ * Klasa nasledjuje klasu koja predstavlja apstraktnu sistemsku operaciju.
+ * 
+ * @author Dragica Ljubisavljevic
+ * @version 1.0
+ *
+ */
 public class PromeniPodatkeBaletskeGrupeSO extends ApstraktnaSO{
 	
 	public PromeniPodatkeBaletskeGrupeSO() {
@@ -26,7 +36,11 @@ public class PromeniPodatkeBaletskeGrupeSO extends ApstraktnaSO{
 
         }
     }
-
+    /**
+     * Metoda u kojoj se poziva operacija za promenu odgovarajuceg sloga u bazi podataka i izvrsava 
+     * odgovarajuci upit nad bazom podataka.
+     * 
+     */
     @Override
     protected void executeOperation(Object param) throws Exception {
        repository.promeni((ApstraktniDomenskiObjekat)param);
