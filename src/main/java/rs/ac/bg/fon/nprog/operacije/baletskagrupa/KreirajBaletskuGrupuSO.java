a48382986ab0219560ea52e172909055cf27bf2a
dragicalj <gagaljubisavljevic111@gmail.com>
2022-09-02 01:50:08 +0200

    Dokumentovana klasa KreirajBaletskuGrupuSO

--- src/main/java/rs/ac/bg/fon/nprog/operacije/baletskagrupa/KreirajBaletskuGrupuSO.java
+++ src/main/java/rs/ac/bg/fon/nprog/operacije/baletskagrupa/KreirajBaletskuGrupuSO.java
@@ -4,15 +4,27 @@ import rs.ac.bg.fon.nprog.domen.ApstraktniDomenskiObjekat;
 import rs.ac.bg.fon.nprog.domen.BaletskaGrupa;
 import rs.ac.bg.fon.nprog.operacije.ApstraktnaSO;
 import rs.ac.bg.fon.nprog.repository.Repository;
-
+/**
+ * 
+ * Klasa koja predstavlja sistemsku operaciju za kreiranje baletske grupe. Baletska grupa se pamti
+ * u bazi podataka, a rezultat operacije je indeks kreirane baletske grupe.
+ * 
+ * Klasa nasledjuje klasu koja predstavlja apstraktnu sistemsku operaciju.
+ * 
+ * @author Dragica Ljubisavljevic
+ * @version 1.0
+ *
+ */
 public class KreirajBaletskuGrupuSO extends ApstraktnaSO {
-
+	/**
+	 * Promenljiva u kojoj se cuva indeks kreirane grupe.
+	 */
     long indeks = -1;
 
     public KreirajBaletskuGrupuSO() {
         super();
     }
-    
+   
     public KreirajBaletskuGrupuSO(Repository repository) {
 		super(repository);
 	}
@@ -28,7 +40,13 @@ public class KreirajBaletskuGrupuSO extends ApstraktnaSO {
 
         }
     }
-
+    /**
+     * Metoda u kojoj se poziva operacija za kreiranje sloga u bazi podataka i izvrsava odgovarajuci
+     * upit nad bazom podataka.
+     * 
+     * Rezultat upita je indeks kreirane baletske grupe.
+     * 
+     */
     @Override
     protected void executeOperation(Object param) throws Exception {
         indeks = repository.kreirajSlog((ApstraktniDomenskiObjekat) param);
