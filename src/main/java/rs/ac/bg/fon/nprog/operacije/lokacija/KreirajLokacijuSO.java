fcb01edf3ad6da738f72f40f38b861273af45920
dragicalj <gagaljubisavljevic111@gmail.com>
2022-09-02 03:13:03 +0200

    Dokumentovane sistemske operacije koje se odnose na lokaciju

--- src/main/java/rs/ac/bg/fon/nprog/operacije/lokacija/KreirajLokacijuSO.java
+++ src/main/java/rs/ac/bg/fon/nprog/operacije/lokacija/KreirajLokacijuSO.java
@@ -5,8 +5,21 @@ import rs.ac.bg.fon.nprog.domen.Lokacija;
 import rs.ac.bg.fon.nprog.operacije.ApstraktnaSO;
 import rs.ac.bg.fon.nprog.repository.Repository;
 
+/**
+ * 
+ * Klasa koja predstavlja sistemsku operaciju za kreiranje lokacije. Lokacija se pamti
+ * u bazi podataka, a rezultat operacije je indeks kreirane lokacije.
+ * 
+ * Klasa nasledjuje klasu koja predstavlja apstraktnu sistemsku operaciju.
+ * 
+ * @author Dragica Ljubisavljevic
+ * @version 1.0
+ *
+ */
 public class KreirajLokacijuSO extends ApstraktnaSO{
-    
+	/**
+	 * Promenljiva u kojoj se cuva indeks kreirane lokacije.
+	 */
     long indeks=-1;
 
     public KreirajLokacijuSO() {
@@ -23,7 +36,13 @@ public class KreirajLokacijuSO extends ApstraktnaSO{
             throw new Exception("Prosledjeni objekat nije instanca klase Lokacija!");
         }
     }
-
+    /**
+     * Metoda u kojoj se poziva operacija za kreiranje sloga u bazi podataka i izvrsava odgovarajuci
+     * upit nad bazom podataka.
+     * 
+     * Rezultat upita je indeks kreirane lokacije.
+     * 
+     */
     @Override
     protected void executeOperation(Object param) throws Exception {
         indeks=repository.kreirajSlog((ApstraktniDomenskiObjekat)param);
--- src/main/java/rs/ac/bg/fon/nprog/operacije/lokacija/VratiSveLokacijeSO.java
+++ src/main/java/rs/ac/bg/fon/nprog/operacije/lokacija/VratiSveLokacijeSO.java
@@ -7,8 +7,24 @@ import rs.ac.bg.fon.nprog.domen.Lokacija;
 import rs.ac.bg.fon.nprog.operacije.ApstraktnaSO;
 import rs.ac.bg.fon.nprog.repository.Repository;
 
+/**
+ * 
+ * Klasa koja predstavlja sistemsku operaciju za prikaz informacija svih lokacija na kojima se mogu 
+ * odrzavati nastupi baletskog studija. Iz baze podataka ucitavaju se podaci o lokacijama i rezultat 
+ * operacije je lista objekata klase Lokacija.
+ * 
+ * 
+ * Klasa nasledjuje klasu koja predstavlja apstraktnu sistemsku operaciju.
+ * 
+ * @author Dragica Ljubisavljevic
+ * @version 1.0
+ *
+ */
 public class VratiSveLokacijeSO extends ApstraktnaSO{
     
+	/**
+     * Promenljiva u kojoj ce se cuvati lokacije kao rezultat operacije.
+     */
     private List<ApstraktniDomenskiObjekat> listaLokacija;
 
     public VratiSveLokacijeSO() {
@@ -23,6 +39,10 @@ public class VratiSveLokacijeSO extends ApstraktnaSO{
     protected void precondition(Object param) throws Exception {
     }
 
+    /**
+     * Metoda u kojoj se poziva operacija za vracanje svih lokacija iz baze podataka.
+     * Rezultat operacije su lokacije sa svim podacima.
+     */
     @Override
     protected void executeOperation(Object param) throws Exception {
         listaLokacija= repository.vratiSve((ApstraktniDomenskiObjekat) new Lokacija());
