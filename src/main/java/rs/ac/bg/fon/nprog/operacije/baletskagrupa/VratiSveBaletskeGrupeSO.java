c1d8592a96fd97c5f46f17fabda8bff39b3c271e
dragicalj <gagaljubisavljevic111@gmail.com>
2022-09-02 02:04:37 +0200

    Dokumentovana klasa VratiSveBaletskeGrupeSO

--- src/main/java/rs/ac/bg/fon/nprog/operacije/baletskagrupa/VratiSveBaletskeGrupeSO.java
+++ src/main/java/rs/ac/bg/fon/nprog/operacije/baletskagrupa/VratiSveBaletskeGrupeSO.java
@@ -8,8 +8,23 @@ import rs.ac.bg.fon.nprog.domen.Koreograf;
 import rs.ac.bg.fon.nprog.operacije.ApstraktnaSO;
 import rs.ac.bg.fon.nprog.repository.Repository;
 
+/**
+ * 
+ * Klasa koja predstavlja sistemsku operaciju za prikaz informacija svih baletska grupa koje postoje
+ * u baletskom studiju. Iz baze podataka ucitavaju se podaci o baletskim  grupama i rezultat 
+ * operacije je lista objekata klase BaletskaGrupa.
+ * 
+ * 
+ * Klasa nasledjuje klasu koja predstavlja apstraktnu sistemsku operaciju.
+ * 
+ * @author Dragica Ljubisavljevic
+ * @version 1.0
+ *
+ */
 public class VratiSveBaletskeGrupeSO extends ApstraktnaSO{
-    
+	/**
+     * Promenljiva u kojoj ce se cuvati baletske grupe kao rezultat upita.
+     */
     private List<ApstraktniDomenskiObjekat> listaBaletskihGrupa;
     
     public VratiSveBaletskeGrupeSO() {
@@ -23,7 +38,11 @@ public class VratiSveBaletskeGrupeSO extends ApstraktnaSO{
     @Override
     protected void precondition(Object param) throws Exception {
     }
-
+    
+    /**
+     * Metoda u kojoj se poziva operacija za vracanje svih baletskih grupa iz baze podataka.
+     * Rezultat operacije su baletske grupe sa svim podacima.
+     */
     @Override
     protected void executeOperation(Object param) throws Exception {
         listaBaletskihGrupa=repository.vratiSve((ApstraktniDomenskiObjekat)new BaletskaGrupa());
